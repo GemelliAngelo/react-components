ba3438d3609e3a7d96280fd9274cea96e9f9b237
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-28 15:00:16 +0100

    addc button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="text-center p-4">
+    <header className="text-center p-3">
       <div className="container">
         <h1>Il mio Blog</h1>
       </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ export default function Main() {
   return (
     <main>
       <div className="container">
-        <div className="d-flex justify-content-center py-3">
+        <div className="d-flex justify-content-center">
           <div className="card">
             <img
               src="src\assets\img\placeholder.png"
@@ -11,12 +11,15 @@ export default function Main() {
             />
             <div className="card-body">
               <h2 className="card-title py-2">Titolo del post</h2>
-              <p className="card-text py-2">
+              <p className="card-text">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
                 voluptatem facilis, debitis eum eveniet eligendi dolore velit,
                 accusamus, veniam omnis harum? Incidunt, similique! Tempore
                 perspiciatis voluptatem quod, minus numquam repudiandae!
               </p>
+              <button type="button" className="btn btn-warning py-2 px-3">
+                LEGGI DI PIÙ
+              </button>
             </div>
           </div>
         </div>
