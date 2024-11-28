2feecd207af403584490521d55a7f9932fb3826a
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-28 14:28:00 +0100

    add header components + fonts family

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
