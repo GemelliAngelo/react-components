e31f97db51af08b506564f778db6f1f125cb2399
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-28 14:54:52 +0100

    add main style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,22 @@ export default function Main() {
   return (
     <main>
       <div className="container">
-        <div className="card">
-          <img src="..." alt="example" className="card-img-top" />
-          <div className="card-body">
-            <h2 className="card title">Titolo del post</h2>
-            <p className="card-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
-              voluptatem facilis, debitis eum eveniet eligendi dolore velit,
-              accusamus, veniam omnis harum? Incidunt, similique! Tempore
-              perspiciatis voluptatem quod, minus numquam repudiandae!
-            </p>
+        <div className="d-flex justify-content-center py-3">
+          <div className="card">
+            <img
+              src="src\assets\img\placeholder.png"
+              alt="example"
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h2 className="card-title py-2">Titolo del post</h2>
+              <p className="card-text py-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+                voluptatem facilis, debitis eum eveniet eligendi dolore velit,
+                accusamus, veniam omnis harum? Incidunt, similique! Tempore
+                perspiciatis voluptatem quod, minus numquam repudiandae!
+              </p>
+            </div>
           </div>
         </div>
       </div>
